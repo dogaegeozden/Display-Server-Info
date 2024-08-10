916799eb4826382a4c43162d3abb012aa05ed1d0
elephant <elephant@localhost.localdomain>
2024-08-10 16:17:05 +0000

    Upgrade

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
     <body>
         <?php
         $server_infos = array(
-            "File Name:" => "PHP_SELF", 
-            "Host Header:" => "HTTP_HOST", 
-            "Completer URL:" => "HTTP_REFERER", 
-            "User Agent:" => "HTTP_USER_AGENT", 
-            "Script Name:" => "SCRIPT_NAME", 
-            "Version of The Common Gateway Interface:" => "GATEWAY_INTERFACE", 
-            "Server Address:" => "SERVER_ADDR", 
-            "Server Software:" => "SERVER_SOFTWARE", 
-            "Server Protocol:" => "SERVER_PROTOCOL",
+            "File Name" => "PHP_SELF", 
+            "Host Header" => "HTTP_HOST", 
+            "Completer URL" => "HTTP_REFERER", 
+            "User Agent" => "HTTP_USER_AGENT", 
+            "Script Name" => "SCRIPT_NAME", 
+            "Version of The Common Gateway Interface" => "GATEWAY_INTERFACE", 
+            "Server Address" => "SERVER_ADDR", 
+            "Server Software" => "SERVER_SOFTWARE", 
+            "Server Protocol" => "SERVER_PROTOCOL",
         );
         ?>
         <div>
             <ul>
             <?php
             foreach($server_infos as $key => $value) {
-                echo "<li>" . $key . " " . $_SERVER[$value] . "</li>";
+                echo "<li>" . $key . ":" . " " . $_SERVER[$value] . "</li>";
             }
             ?>
             </ul>
